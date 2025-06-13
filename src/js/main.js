2fbb58c1b4773600ef88d5a9dedcc7a34f97cc70
MatSosa4 <villento489@outlook.com>
2025-06-13 11:31:35 -0500

    separated graphs windows

---
+++ src/js/main.js
@@ -0,0 +1,105 @@
+// Data Structures: https://www.chartjs.org/docs/latest/general/data-structures.html
+import Chart from 'chart.js/auto';
+
+const barChart = document.getElementById('barChart');  // https://www.chartjs.org/docs/latest/charts/bar.html
+const stackedChart = document.getElementById('stackedChart');  // https://www.chartjs.org/docs/latest/charts/bar.html#stacked-bar-chart
+const groupChart = document.getElementById('groupChart');
+const stackedGroupChart = document.getElementById('stackedGroupChart');
+
+new Chart(barChart, {
+  type: 'bar',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+
+new Chart(stackedChart, {
+  type: 'bar',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [
+    {
+      label: '# of Votes',
+      data: [10, 9, 8, 3],
+      borderWidth: 1
+    },
+    {
+      label: '#2 of Votes',
+      data: [6, 5, 4, 3],
+      borderWidth: 1
+    },
+    ]
+  },
+  options: {
+    scales: {
+      x: {
+        stacked: true
+      },
+      y: {
+        stacked: true
+      }
+    }
+  }
+});
+
+new Chart(groupChart, {
+  type: 'bar',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green'],
+    datasets: [
+      {
+        label: '# of Votes',
+        data: [10, 9, 8, 7],
+        borderWidth: 1
+      },
+      {
+        label: '#2 of Votes',
+        data: [6, 5, 4, 3],
+        borderWidth: 1
+      },
+    ]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+
+new Chart(stackedGroupChart, {
+  type: 'bar',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green'],
+    datasets: [
+      {
+        label: 'Dataset 1',
+        data: [1, 2, 3, 4],
+        stack: 'Stack 0',
+      },
+      {
+        label: 'Dataset 2',
+        data: [5, 6, 7, 8],
+        stack: 'Stack 0',
+      },
+      {
+        label: 'Dataset 3',
+        data: [9, 10, 11, 12],
+        stack: 'Stack 1',
+      },
+    ]
+  }
+});
